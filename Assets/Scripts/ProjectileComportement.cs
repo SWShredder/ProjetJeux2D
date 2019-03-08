2970dd644a7d559075a37dcd825892ba36dc3459
Yanik Sweeney <0413960@cstjean.qc.ca>
2019-03-08 13:12:20 -0500

    AJOUT librairie extension pour editeur ET fonctionnalité portée à ProjectileComportement

--- Assets/Scripts/ProjectileComportement.cs
+++ Assets/Scripts/ProjectileComportement.cs
@@ -9,6 +9,7 @@
  */
 using System;
 using System.Collections;
+using MyBox;
 using UnityEngine;
 
 public class ProjectileComportement : MonoBehaviour
@@ -19,13 +20,19 @@ public class ProjectileComportement : MonoBehaviour
     public GameObject Parent { set; get; }
 
     [SerializeField]
+    protected bool estProjectile = true;
+
+    [SerializeField, ConditionalField("estProjectile", true)]
     protected float vitesse = 2f;
+
+    [SerializeField, ConditionalField("estProjectile", false)]
+    protected float duréeVie = 5f;
 
     [SerializeField]
     protected int dégats = 1;
 
     [SerializeField]
-    protected float duréeVie = 5f;
+    protected float portée = 6f;
 
     [SerializeField]
     protected float délaiDégat = 0f;
@@ -37,7 +44,7 @@ public class ProjectileComportement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, duréeVie);
+        Destroy(this.gameObject, DuréeVie());
         corpsPhysique = GetComponent<Rigidbody2D>();
         direction = transform.rotation == Quaternion.Euler(0f, 0f, 0f) ? Vector2.right : Vector2.left;
     }
@@ -55,11 +62,14 @@ public class ProjectileComportement : MonoBehaviour
         if (!EstTransperçant) Destroy(this.gameObject);
     }
 
+    protected float DuréeVie() => vitesse > 0 ? portée / vitesse : duréeVie;
 
     protected void DéclencherDégats(GameObject source) => source.GetComponent<ControleurRpg>().StartCoroutine(
         Coroutines.Instance.ActionDiférée(() => source.GetComponent<ControleurRpg>().AppliquerDégats(dégats), délaiDégat));
-    protected bool EstSourceValidePourDommage(GameObject source) => EstSourceEntitéValide(source) && EstSourceComposanteRpgValide(source);
+    protected bool EstSourceValidePourDommage(GameObject source) => EstSourceEntitéValide(source) && EstSourceComposanteRpgValide(source)
+        && !EstSourceTagIdentique(source);
     private bool EstSourceProjectile(GameObject source) => source.CompareTag("Projectile");
+    private bool EstSourceTagIdentique(GameObject source) => source.tag == Parent.tag;
     private bool EstSourceEntitéValide(GameObject source) => !EstSourceProjectile(source)
         && source.GetComponent<ControleurEntité>() != null && !source.GetComponent<ControleurEntité>().EstMort;
     private bool EstSourceComposanteRpgValide(GameObject source) => source.GetComponent<ControleurRpg>() != null;
